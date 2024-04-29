b8bce1f33dd186a0e21599bae206c6a006dc298a
AyaMahfouz <aya.mmahfouz@gmail.com>
2024-04-28 22:27:03 -0400

    added currency component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,29 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { expenses, budget, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+
+        if(newBudget > 20000){
+            alert("You can not set the budget to more than 20000");
+        }
+        else if(newBudget < totalExpenses){
+            alert("You can not reduce the budget value lower than spending");
+        }
+        else{
+            setNewBudget(event.target.value);
+        }
+        
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £     </span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+        <span>Budget: {currency}     </span>
+        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
 export default Budget;
---
+++ src/components/Currency.js
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import "./Currency.css";
+
+const Currency = () =>{
+    const { dispatch} = useContext(AppContext);
+    
+    const currArray = [
+        {name: "Pound", symbol:"£"},
+        {name: "Dollar", symbol:"$"},
+        {name: "Euro", symbol:"€"},
+        {name: "Ruppee", symbol:"₹"},
+    ];
+    
+
+    const handleCurrencyChange = (event) => {
+
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event.target.value,
+        });
+    }
+
+    return(
+        <div className="alert alert-cur">
+            <span>Currency (
+                <select id="selectCurr" className="select" onChange={handleCurrencyChange}>
+                    {currArray.map((curr) =>{
+                        return <option value={curr.symbol} key={curr.name}>{curr.symbol}: {curr.name}</option>
+                    })}
+                </select>
+            )</span>
+        </div>
+    )
+}
+
+export default Currency;
